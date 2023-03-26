1d3c7b4999dce34766e80922a35e51deb3b3c687
Ing Orlando Barragan <nandito.barragan2016@gmail.com>
2023-03-26 18:29:14 -0500

    Proyecto final

---
+++ src/main/java/edu/udea/main/controlador/FrontendController.java
@@ -0,0 +1,95 @@
+package edu.udea.main.controlador;
+
+import edu.udea.main.model.ROLES;
+import edu.udea.main.model.Usuario;
+import edu.udea.main.service.GestorUsuarioInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class FrontendController {
+    @Autowired
+    private GestorUsuarioInterface gestorUsuario;
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(Model model){
+        model.addAttribute("forUsuario", new Usuario());
+        return "login";
+    }
+    @GetMapping("/welcome")
+    public String getWelcome(Model model) {
+        model.addAttribute("usuarios",gestorUsuario.getUsuarios());
+        return "/welcome";
+    }
+   @PostMapping("/login")
+    public String login(@ModelAttribute("formUsuario") Usuario usuario){
+       System.out.println(usuario);
+       return "redirect:/welcome";
+   }
+    @GetMapping("/addUser")
+    public String getUddUser(Model model){
+        model.addAttribute("user", new Usuario());
+        model.addAttribute("ROLES", ROLES.values());
+        return "add-user";
+    }
+    @PostMapping("/usuario/front")
+    public String postUsuario(@ModelAttribute("user") Usuario usuario_parametro){
+        try {
+            System.out.println(usuario_parametro);
+            String mensaje = gestorUsuario.setUsuario(usuario_parametro);
+            return "redirect:/welcome";
+
+        } catch (Exception e) {
+            return "redirect:/error";
+        }
+    }
+    @GetMapping("/updateUser")
+    public String getUpdateUser(Model model){
+        return "update-user";
+    }
+
+    @GetMapping("/usuario/front/{id}")
+    public String getusuario(@PathVariable String id, Model model){
+        try {
+            System.out.println(id);
+            System.out.println(gestorUsuario.getUsuario(id));
+            model.addAttribute("userUpdate",gestorUsuario.getUsuario(id));
+            model.addAttribute("ROLES", ROLES.values());
+            return "update-user";
+        } catch (Exception e) {
+            return "redirect:/error";
+        }
+
+    }
+
+    @DeleteMapping("/usuario/front/{id}")
+    public String deleteUser(@PathVariable String id, Model model){
+        try{
+            gestorUsuario.deleteUsuario(id);
+            return "redirect:/welcome";
+        }catch (Exception e){
+            return "redirect:/error";
+        }
+
+
+    }
+    @PutMapping("/usuario/front/update")
+    public String putUsuario(@ModelAttribute("userUpdate") Usuario usuario){
+        try {
+            System.out.println(usuario);
+            gestorUsuario.updateUsuario(usuario,usuario.getNombreUsuario());
+            return "redirect:/welcome";
+        } catch (Exception e) {
+            return "redirect:/error";
+        }
+
+    }
+
+}
---
+++ src/main/java/edu/udea/main/controlador/UsuarioControlador.java
@@ -0,0 +1,114 @@
+package edu.udea.main.controlador;
+
+
+import edu.udea.main.service.GestorUsuarioInterface;
+import edu.udea.main.model.ObjetoRespuesta;
+import edu.udea.main.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class UsuarioControlador {
+
+    @Autowired
+    private GestorUsuarioInterface gestorUsuario;
+
+    @GetMapping("/usuario/usuarios")
+    public ResponseEntity<List<Usuario>> getUsuarios(){
+        return new ResponseEntity<>(gestorUsuario.getUsuarios(), HttpStatus.OK);
+    }
+
+    @GetMapping("/usuario")
+    public ResponseEntity<Object> getUsuario(@RequestParam String id){
+        try {
+            Usuario usuario = gestorUsuario.getUsuario(id);
+            return new ResponseEntity<>(usuario,HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("/usuario/{id}")
+    public ResponseEntity<String> getUsuarioPath(@PathVariable String id){
+        return new ResponseEntity<>(id,HttpStatus.OK);
+    }
+
+    @PostMapping("/usuario")
+    public ResponseEntity<String> postUsuario(@RequestBody Usuario usuario_parametro){
+
+        try {
+            String mensaje = gestorUsuario.setUsuario(usuario_parametro);
+            return new ResponseEntity<>(mensaje,HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+    }
+
+
+    @PutMapping("/usuario/{id}")
+    public ResponseEntity<ObjetoRespuesta> putUsuario(@RequestBody Usuario usuario_update, @PathVariable String id){
+
+        try {
+            Usuario usuario_bd = gestorUsuario.updateUsuarioAll(usuario_update, id);
+            return new ResponseEntity<>(new ObjetoRespuesta("Actualización Exitosa",usuario_bd),HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ObjetoRespuesta(e.getMessage(),null),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PatchMapping("/usuario/{id}")
+    public ResponseEntity<ObjetoRespuesta> patchUsuario(@RequestBody Usuario usuario_update, @PathVariable String id ){
+
+        try {
+            Usuario usuario_bd = gestorUsuario.updateUsuario(usuario_update, id);
+            return new ResponseEntity<>(new ObjetoRespuesta("Actualización Exitosa",usuario_bd),HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(new ObjetoRespuesta(e.getMessage(),null),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/usuario/{id}")
+    public ResponseEntity<ObjetoRespuesta> deleteUsuario(@PathVariable String id){
+
+        try {
+            String mensaje = gestorUsuario.deleteUsuario(id);
+
+            return new ResponseEntity<>(new ObjetoRespuesta(mensaje,null),HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ObjetoRespuesta(e.getMessage(),null),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
